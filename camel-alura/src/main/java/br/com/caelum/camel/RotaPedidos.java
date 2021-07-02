1e305f23523be130a3f8dcec4c3cc0754f3f5c2b
Damaris-PC <damisoliveira@gmail.com>
2021-07-02 14:20:07 -0300

    Initial commit

---
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,28 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:D:\\projeto-camel\\camel-alura\\pedidos?delay=5s&noop=true")
+						.log("${id}")
+						.marshal()
+						.xmljson()
+						.log("${body}")
+						.setHeader("CamelFileName",simple("${file:name.noext}.json"))
+						.to("file:D:\\projeto-camel\\camel-alura\\saida");
+			}
+		});
+		context.start(); //aqui camel realmente começa a trabalhar
+		Thread.sleep(20000); //esperando um pouco para dar um tempo para camel
+	}
+}
+
